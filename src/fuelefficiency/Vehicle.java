398d09d06b55992b617fff0bcdd6eac8471c7b81
etwat3497 <atwatt67@gmail.com>
2016-11-01 17:44:03 -0400

    Update Vehicle.java

--- src/fuelefficiency/Vehicle.java
+++ src/fuelefficiency/Vehicle.java
@@ -6,6 +6,10 @@
 
 package fuelefficiency;
 
+//Add imports
+import java.text.DecimalFormat;
+
+
 /**
  *
  * @author etwat3497
@@ -13,11 +17,23 @@ package fuelefficiency;
 public abstract class Vehicle {
     //Declare global variables
     public double litersOfFuel, distance;
+    DecimalFormat x = new DecimalFormat("##.##");
       
     //Create an abstract method to get distance
     public abstract double getDistance();
          
     //Create an abstract method to output string
     public abstract String toString();
-
+ 
+    
+  /**
+   * pre: double unrounded distance travelled 
+   * post: double rounded distance travelled
+   * Helper method to round distances
+   */
+    public double rounder(double unroundedDistance){
+        DecimalFormat x = new DecimalFormat("00.00");
+        double roundedDistance = Double.parseDouble(x.format(unroundedDistance));
+        return roundedDistance;
+    }
 }
